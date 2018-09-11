0fa982596cabd849d3ed659b98c41dbc4cc2b89d
Romantic <595159090004@dpu.ac.th>
2018-09-12 06:49:25 +0700

    aws lamda function -> rank emoji

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,52 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: ''
+    };
+  }
+
+  generateEmoji = entries => {
+    // get from aws lamda
+    fetch(
+      `https://xxwrx76qcj.execute-api.ap-southeast-1.amazonaws.com/prod/rank?rank=${entries}`
+    )
+      .then(res => res.json())
+      .then(data => this.setState({ emoji: data.input }))
+      .catch(console.log);
+  };
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, PrevState) {
+    if (
+      prevProps.entries === this.props.entries &&
+      prevProps.name === this.props.name
+    ) {
+      return null;
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="white f3">
+          {`${
+            this.props.name
+          }, your current entry count is...`}
+        </div>
+        <div className="white f1">{this.props.entries}</div>
+        <div className="white f3">{`Rank Badge: ${
+          this.state.emoji
+        }`}</div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Rank;
